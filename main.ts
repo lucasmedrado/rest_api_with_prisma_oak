824157107d1519fb3e9be73411cba10167fc81ce
Lucas Medrado <lucasmedrado7@gmail.com>
2023-04-07 13:32:00 -0300

    primeiro commit

---
+++ main.ts
@@ -0,0 +1,15 @@
+// CONFIGURAR O APLICATIVO
+
+/*
+Rodando prisma init com o Deno:
+deno run --allow-read --allow-env --allow-write npm:prisma@^4.5 init
+-> Isso vai gerar prisma/schema.prisma
+
+Em seguida, vamos criar o esquema do banco de dados:
+deno run -A npm:prisma@^4.5 db push
+*/
+
+/*
+COMANDO PARA ENTRAR NA PASTA ESPECÍFICA:
+_4_modulos_node_npm/_4.5_como_guias_com_npm/_4.5.1_prisma/rest_api_with_prisma_oak
+*/
